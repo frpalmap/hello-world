7d0a5b99fe27c05a481fed8b8b20db6e606052b5
Fidel Palma <frpalma2@gmail.com>
2020-08-01 22:48:11 -0400

    Esta es una prueba para hacer un commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+//Primer programa de prueba
+#include <iostream>
+using namespace std;
+
+int main(){
+    int NumToShow;
+    cout<<"Inserte un entero y presione enter:";
+    cin>>NumToShow;
+    cout<<"Hello World"; 
+    cout<<NumToShow;
+    return 0;
+}
