2fd7f8d934a8b3a4fb91e005715095dd354ab2b1
Fidel Palma <frpalma2@gmail.com>
2020-08-03 21:52:25 -0400

    Para irme

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,11 +2,33 @@
 #include <iostream>
 using namespace std;
 
+
+int suich(){
+    int D;
+    cout<< "1 para una Frase, 2 para otra:";
+    cin >> D;
+    switch(D){
+        case 1:{
+            cout<<"Presionaste 1" <<endl;
+            break;
+        }
+        case 2:{
+            cout<<"Presionaste 2" <<endl;
+            break;
+        }
+    }
+    return D;
+}
+
 int main(){
-    int NumToShow;
-    cout<<"Inserte un entero y presione enter:";
-    cin>>NumToShow;
-    cout<<"Hello World:",  
-    cout<<NumToShow;
+    int A, B, C;
+    cout<< "Hello World" <<"Inserte dos enteros y presione enter luego de cada uno:" << endl ;
+    cin>>A >> B;
+    C=A+B;
+    cout<<"La suma es:" <<C <<endl;
+    C=suich();
+    cout<<"Y la respuesta al switch fue: " << C <<endl;
+    
+
     return 0;
 }
